64f784e8c6e418e0bb3d72b1021bf4f8eee990fd
Matteo Marinelli <mmarinel@e4r8p4.42roma.it>
2022-10-04 19:55:15 +0200

    ex01: wip

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,104 +5,52 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/04 14:44:14 by mmarinel          #+#    #+#             */
-/*   Updated: 2022/09/23 11:54:34 by mmarinel         ###   ########.fr       */
+/*   Created: 2022/10/04 19:01:51 by mmarinel          #+#    #+#             */
+/*   Updated: 2022/10/04 19:54:40 by mmarinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
-#include "Form.hpp"
+# include "types.hpp"
+# include "../colors.hpp"
 
-#include <fstream>
-#include <iostream>
-#include <string>
+# include <iostream>
+# include <string>
+# include <cstdint>
 
-static Bureaucrat*	read_bureaucrat( void );
-static Form*		read_form( void );
+static uintptr_t	serialize(Data* ptr);
+static Data*		deserialize(uintptr_t raw);
 //* end of static declarations
 
-int	main()
+int	main( void )
 {
-	Bureaucrat	*bur = nullptr;
-	Form		*form = nullptr;
-	bool		repeat;
+	Data	data = {"Clegg", "Corporal"};
 
-	std::cin.exceptions(std::ios_base::badbit | std::ios_base::eofbit);
-	repeat = true;
-	while (repeat)
-	{
-		try {
-			bur = read_bureaucrat();
-			std::cout << std::endl;
-			form = read_form();
-			bur->signForm(*form);
-			repeat = false;
-		}
-		catch (std::istream::failure &e) {
-			std::cout << BOLDRED << "eof caught or stream broken" << RESET << std::endl;
-			repeat = false;
-		}
-		catch (std::exception &e) {
-			std::cout << "\npress any key to retry..." << std::endl;
-			getchar();
-		}
-		_delete(bur);
-		_delete(form);
-	}
+	std::cout << "Printing structure before pointer magic" << std::endl;
+	std::cout << data << std::endl;
+	std::cout << "Printing structure after pointer magic" << std::endl;
+	Data*	newPtr = deserialize(serialize(&data));
+	std::cout << data << std::endl;
+
+	if (newPtr == &data)
+		std::cout << "Pointer Check: " << GREEN << "OK" << RESET << std::endl;
+	else
+		std::cout << "Pointer Check: " << RED << "NOT OK" << RESET << std::endl;
 	return 0;
 }
 
-static Bureaucrat*	read_bureaucrat( void )
+static uintptr_t	serialize(Data* ptr)
 {
-	Bureaucrat*	bur = nullptr;
-	bool		repeat;
-	std::string	bur_name;
-	int			bur_grade;
-
-	repeat = true;
-	while (repeat)
-	{
-		read_string(bur_name, "enter bureaucrat name");
-		read_input(&bur_grade, int, "insert bureaucrat grade");
-		try {
-			bur = new Bureaucrat(bur_name, bur_grade);
-			repeat = false;
-		}
-		catch (std::exception& e) {
-			std::cout << "Invalid bureaucrat data: " << e.what() << std::endl;
-			std::cout << "Press any key to retry...";
-			std::getchar();
-			std::cout  << std::endl;
-		}
-	}
-	return (bur);
+	return (reinterpret_cast<uintptr_t>(ptr));
 }
 
-static Form*	read_form( void )
+static Data*		deserialize(uintptr_t raw)
 {
-	Form*		form = nullptr;
-	bool		repeat;
-	std::string	name;
-	int			sign_grade;
-	int			exec_grade;
+	return (reinterpret_cast<Data*>(raw));
+}
 
-	repeat = true;
-	while (repeat)
-	{
-		read_string(name, "choose form name");
-		read_input(&sign_grade, int, "choose form grade to sign");
-		read_input(&exec_grade, int, "choose form grade to execute");
-		try
-		{
-			form = new Form(name, false, sign_grade, exec_grade);
-			repeat = false;
-		}
-		catch(const std::exception& e)
-		{
-			std::cout << "Invalid Form data: " << e.what();
-			std::getchar();
-			std::cout  << std::endl;
-		}
-	}
-	return (form);
+std::ostream&	operator<<( std::ostream& stream, const Data& obj)
+{
+	stream << obj.rank << " " << obj.name;
+
+	return (stream);
 }
--- ex01/types.hpp
+++ ex01/types.hpp
@@ -6,17 +6,21 @@
 /*   By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 16:50:40 by mmarinel          #+#    #+#             */
-/*   Updated: 2022/09/20 11:01:38 by mmarinel         ###   ########.fr       */
+/*   Updated: 2022/10/04 19:12:22 by mmarinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
 #define TYPES_H
 
-//**             FORWARD DECLARATION FILE                   **//
+# include <iostream>
 
-class Grade;
-class Bureaucrat;
-class Form;
+typedef struct s_Data
+{
+	const char*	name;//* Clegg
+	const char*	rank;//* Corporal (caporale)
+}	Data;
+
+std::ostream&	operator<<( std::ostream& stream, const Data& obj);
 
 #endif /* TYPES_H */
