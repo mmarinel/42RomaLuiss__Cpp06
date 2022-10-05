cd9312954ecb90cf57a5d4926f0484cb92de8be5
Matteo Marinelli <mmarinel@e4r6p3.42roma.it>
2022-10-05 16:10:38 +0200

    ex02: done

--- ex02/C.hpp
+++ ex02/C.hpp
@@ -6,7 +6,7 @@
 /*   By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 14:34:14 by mmarinel          #+#    #+#             */
-/*   Updated: 2022/10/05 16:03:13 by mmarinel         ###   ########.fr       */
+/*   Updated: 2022/10/05 16:04:24 by mmarinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ class C : public Base
 };
 
 std::ostream&	operator<<( std::ostream& stream, const C& obj );
+
 #endif /* C_H */
